391717e829e1ed2c5d57f49c4d8ce50ffb2f7dc4
Ryan Dill <ryand1127@gmail.com>
2021-06-17 17:03:13 -0400

    added tests

--- knexfile.js
+++ knexfile.js
@@ -19,6 +19,26 @@ module.exports = {
         // runs after a connection is made to the sqlite engine
         conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
       },
+   }
+  },
+
+  testing: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/test.db3'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: { // needed to enable foreign keys in sqlite
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
   },
 
 
